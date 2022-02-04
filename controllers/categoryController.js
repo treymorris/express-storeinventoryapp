5873fa2a7af02739376a448ec6f1e01391629065
Trey Morris <treymorrisit@hotmail.com>
2022-02-04 17:04:21 -0500

    added category delete

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -107,13 +107,59 @@ exports.category_create_post =  [
 //};
 
 // Display Genre delete form on GET.
-exports.category_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre delete GET');
+//exports.category_delete_get = function(req, res) {
+//    res.send('NOT IMPLEMENTED: Genre delete GET');
+//};
+exports.category_delete_get = function(req, res, next) {
+
+    async.parallel({
+        category: function(callback) {
+            Category.findById(req.params.id).exec(callback)
+        },
+        category_products: function(callback) {
+            Product.find({ 'category': req.params.id }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.category==null) { // No results.
+            res.redirect('/inventory/categories');
+        }
+        // Successful, so render.
+        res.render('category_delete', { title: 'Delete Category', category: results.category, category_products: results.category_products } );
+    });
+
 };
 
 // Handle Genre delete on POST.
-exports.category_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre delete POST');
+//exports.category_delete_post = function(req, res) {
+//    res.send('NOT IMPLEMENTED: Genre delete POST');
+//};
+exports.category_delete_post = function(req, res, next) {
+
+    async.parallel({
+        category: function(callback) {
+          Category.findById(req.body.categoryid).exec(callback)
+        },
+        categories_products: function(callback) {
+          Product.find({ 'category': req.body.categoryid }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        // Success
+        if (results.categories_books.length > 0) {
+            // Author has books. Render in same way as for GET route.
+            res.render('category_delete', { title: 'Delete Category', category: results.category, category_books: results.categories_products } );
+            return;
+        }
+        else {
+            // Author has no books. Delete object and redirect to the list of authors.
+            Category.findByIdAndRemove(req.body.categoryid, function deleteCategory(err) {
+                if (err) { return next(err); }
+                // Success - go to author list
+                res.redirect('/inventory/categories')
+            })
+        }
+    });
 };
 
 // Display Genre update form on GET.
